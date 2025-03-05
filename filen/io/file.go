84f933048278e15a7b176ef96a8f669cb0511d2d
Enduriel <endur1el@protonmail.com>
2025-03-05 16:34:09 +0000

    feat: add alternative MakeFileInfo for rclone

--- filen/io/file.go
+++ filen/io/file.go
@@ -24,17 +24,23 @@ func MakeInfoFromFile(file *os.File) (FileInfo, error) {
 		return FileInfo{}, fmt.Errorf("stat file: %w", err)
 	}
 	created := GetCreationTime(fileStat)
-	mimeType := mime.TypeByExtension(filepath.Ext(file.Name()))
+	return MakeFileInfo(file.Name(), created, fileStat.ModTime(), file, ""), nil
+}
+
+func MakeFileInfo(path string, created time.Time, lastModified time.Time, data io.Reader, mimeType string) FileInfo {
+	if mimeType == "" {
+		mimeType = mime.TypeByExtension(filepath.Ext(path))
+	}
 	if mimeType == "" {
 		mimeType = "application/octet-stream"
 	} else {
 		mimeType, _, _ = strings.Cut(mimeType, ";")
 	}
 	return FileInfo{
-		Name:         filepath.Base(file.Name()),
+		Name:         filepath.Base(path),
 		MimeType:     mimeType,
 		Created:      created.Round(time.Millisecond),
-		LastModified: fileStat.ModTime().Round(time.Millisecond),
-		Data:         file,
-	}, nil
+		LastModified: lastModified.Round(time.Millisecond),
+		Data:         data,
+	}
 }
