3d781e0db5299a744154912dadf998ad5831100f
Enduriel <endur1el@protonmail.com>
2025-03-05 12:57:28 +0000

    fix: MimeType handling adding charset and not handling unknowns

--- filen/io/file.go
+++ filen/io/file.go
@@ -6,6 +6,7 @@ import (
 	"mime"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -23,9 +24,15 @@ func MakeInfoFromFile(file *os.File) (FileInfo, error) {
 		return FileInfo{}, fmt.Errorf("stat file: %w", err)
 	}
 	created := GetCreationTime(fileStat)
+	mimeType := mime.TypeByExtension(filepath.Ext(file.Name()))
+	if mimeType == "" {
+		mimeType = "application/octet-stream"
+	} else {
+		mimeType, _, _ = strings.Cut(mimeType, ";")
+	}
 	return FileInfo{
 		Name:         filepath.Base(file.Name()),
-		MimeType:     mime.TypeByExtension(filepath.Ext(file.Name())),
+		MimeType:     mimeType,
 		Created:      created.Round(time.Millisecond),
 		LastModified: fileStat.ModTime().Round(time.Millisecond),
 		Data:         file,
